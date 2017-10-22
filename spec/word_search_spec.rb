54b20d086de79abcc0d5fd0bea8562327e72ad5f
machen2 <victoriamachen@gmail.com>
2017-10-21 21:49:12 -0500

    updated word_search_spec to remove a test

--- spec/word_search_spec.rb
+++ spec/word_search_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe WordSearch do
   it "has a version number" do
     expect(WordSearch::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
