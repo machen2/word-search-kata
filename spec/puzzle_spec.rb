b03278d650a60514932e9ce30936fd2adae88b63
machen2 <victoriamachen@gmail.com>
2017-10-22 19:57:41 -0500

    updated gemspec, added passing test for creating new instance of puzzle

--- spec/puzzle_spec.rb
+++ spec/puzzle_spec.rb
@@ -1,7 +1,15 @@
 require "spec_helper"
 
 RSpec.describe WordSearch::Puzzle do 
+    let(:word_search) { WordSearch::Puzzle.new }
+    
     it "is defined as a class" do 
         expect(WordSearch::Puzzle).to be_a(Class)
+    end
+
+    describe "#new" do 
+        it "initializes a new instance of Puzzle" do 
+            expect{WordSearch::Puzzle.new}.to_not raise_error
+        end
     end
 end
