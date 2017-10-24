c3633c63a9bf2ba39da235558a734961248a1cc6
machen2 <victoriamachen@gmail.com>
2017-10-23 19:43:38 -0500

    test and code for parse_file method, it parses the grid from the file into the grid attribute

--- lib/word_search/puzzle.rb
+++ lib/word_search/puzzle.rb
@@ -3,10 +3,14 @@ class WordSearch::Puzzle
 
     def initialize(file_name)
         @file_name = file_name
+        @grid = []
     end
 
     def parse_file
         file = File.readlines(@file_name)
         @word_list = file.shift.strip.split(',')
+        file.each do |line|
+            @grid << line.strip.split(',')
+        end
     end
 end
